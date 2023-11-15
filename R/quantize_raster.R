8e1d4bad64c4a418d0b6642c62d1ed89fdf501f0
Alex <ailich@usf.edu>
2023-11-15 10:52:39 -0500

    Add maxcell for quantization

--- R/quantize_raster.R
+++ R/quantize_raster.R
@@ -6,6 +6,7 @@
 #' @param method quantization method (either "equal range" or "equal prob"). "equal range" quantization will create bins that cover a range of equal size. "equal prob" performs equal probability quantization and will use quantiles to create bins with approximately equal number of samples.
 #' @param min_val minimum value for equal range quantization (if not supplied, the minimum value of the raster is used)
 #' @param max_val maximum value for equal range quantization (if not supplied, the maximum value of the raster is used)
+#' @param maxcell positive integer used to take a regular sample of x if "equal prob" is used (default is Inf)
 #' @param filename character Output filename.
 #' @param overwrite logical. If TRUE, filename is overwritten (default is FALSE).
 #' @return a single layer SpatRaster or RasterLayer with integer values ranging from 0 to n_levels-1
@@ -26,7 +27,7 @@
 #' Hyndman, R.J., Fan, Y., 1996. Sample Quantiles in Statistical Packages. The American Statistician 50, 361–365. https://doi.org/10.1080/00031305.1996.10473566
 #' @export
 
-quantize_raster<- function(r, n_levels, method, min_val=NULL, max_val=NULL, filename= NULL, overwrite = FALSE, wopt=list()){
+quantize_raster<- function(r, n_levels, method, min_val=NULL, max_val=NULL, maxcell=Inf, filename= NULL, overwrite = FALSE, wopt=list()){
   og_class<- class(r)[1]
   if(og_class =="RasterLayer"){
     r<- terra::rast(r) #Convert to SpatRaster
@@ -39,10 +40,12 @@ quantize_raster<- function(r, n_levels, method, min_val=NULL, max_val=NULL, file
     if(is.null(max_val)){max_val<- unlist(terra::global(r, fun = max, na.rm=TRUE))}
     qrules<- seq(min_val, max_val, length.out = (n_levels + 1))
   } else if (method == "equal prob") {
-    qrules<- unlist(terra::global(r, fun = quantile, probs= seq(0,1,length.out = n_levels+1), na.rm=TRUE, type=8))
+    qrules<- unlist(terra::global(r, fun = quantile, probs= seq(0,1,length.out = n_levels+1), na.rm=TRUE, type=8, maxcell=maxcell))
   } else {
     message("Error: method must be 'equal range' or 'equal prob'")
   }
+  qrules[1]<- -Inf
+  qrules[length(qrules)]<- Inf #Make edges infinite in case sample is used which doesn't contain min/max value
   rq <- terra::classify(r, rcl = qrules, include.lowest = TRUE, right = FALSE, wopt=wopt)
   rq<- terra::as.int(rq, wopt=wopt)
 
